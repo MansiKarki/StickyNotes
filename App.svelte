0bd33c22dfa96b0358bf6b7dd1cb008b99ca29d3
MansiKarki <140382919+MansiKarki@users.noreply.github.com>
2023-10-31 18:03:34 +0530

    Add files via upload

---
+++ App.svelte
@@ -0,0 +1,40 @@
+<!-- App.svelte -->
+<script>
+    import StickyNote from './StickyNote.svelte';
+   import Note from './Note.svelte';
+    let notes = [
+      // { id: 1, content: 'Note 1' },Automatically add two notes
+      // { id: 2, content: 'Note 2' },
+          
+    ];
+    let newNoteContent = '';
+    function addNote() {
+      if (newNoteContent.trim() !== '') {
+        notes = [...notes, { id: Date.now(), content: newNoteContent }];
+        newNoteContent = '';
+      }
+    }
+  </script>
+  
+  <div>
+    <div>
+      <textarea bind:value={newNoteContent}></textarea>
+      <button on:click={addNote}>Add Note</button>
+    </div>
+  
+    {#each notes as note (note.id)}
+          <Note>
+              <div class="con" contenteditable>{note.content}</div>
+          </Note>
+    {/each}
+  </div>
+  
+  <style>
+      .con
+      {
+      background: yellow;
+      padding: 20px;
+      width: 200px;
+      color:black;
+      text-align: center;
+  }</style>
---
+++ StickyNotes.svelte
@@ -0,0 +1,18 @@
+<!-- StickyNote.svelte -->
+<script>
+    export let content = '';
+  </script>
+  
+  <div class="sticky-note">
+    {content}
+  </div>
+  
+  <style>
+    .sticky-note {
+      border: 2px solid #000;
+      padding: 20px;
+      margin: 10px;
+          color:black;
+    }
+      
+  </style>
